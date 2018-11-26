aa13fb020008465e928aa8fbf58936c66cd35ef2
Mj. Major Major MJMjMajorMajorMj <techk8@gmail.com>
2018-11-26 15:05:46 -0800

    Cleaned up CSS, added Component to add details later

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Lobby from './Lobby';
-import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
--- src/components/lobby/ActivityDetails.js
+++ src/components/lobby/ActivityDetails.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import '../../styles/Lobby.css'
 
-class ActivityItem extends Component {
+class ActivityDetails extends Component {
     render() {
-        const { activity } = this.props
         return (
             <div>
-                <h1>{activity}</h1>
+                <h1>FUG YOU :DDDDDDDDD</h1>
             </div>
         )
     }
 }
 
-export default ActivityItem;
+export default ActivityDetails;
