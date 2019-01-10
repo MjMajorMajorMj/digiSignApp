8d709dfe84ce5997ee8f5bab8949f538d7c71ce4
Mj. Major Major MJMjMajorMajorMj <techk8@gmail.com>
2019-01-09 17:18:30 -0800

    Created additional base components for solo activity

---
+++ src/components/solo/SoloRight.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+
+class SoloRight extends Component {
+    render() {
+        return (
+            <div className="VidDescDiv">
+                <div className="rightVid">
+                    <h1>Right Vid</h1>
+                </div>
+                <div className="rightLeaderDiv">
+                    <h1>Right Leaderboard</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SoloRight;
